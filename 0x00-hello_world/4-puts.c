1028071084dbe78d07b8ea3a9bef4115750ab071
Mohamed Sabour <mohamedsabour63@gmail.com>
2023-02-16 16:21:51 -0800

    another try

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,1 @@
-#iclude<stdio.h>
-int main()
-{
-//string initialisationchar
-char mystr[] = "Programming is like building a multilingual puzzle;
-puts(mystr); //writing the string to stout
-return 0;
+int puts(const char* Programming is like building a multilingual puzzle);
