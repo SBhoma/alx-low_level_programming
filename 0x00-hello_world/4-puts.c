05387473d65a2bd29b19ae7f152c1446b14823e9
Mohamed Sabour <mohamedsabour63@gmail.com>
2023-02-16 18:31:15 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,6 @@
-int puts(const char* Programming is like building a multilingual puzzle);
+#include <stdio.h>
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle);
+return 0;
+}
