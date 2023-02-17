d81f75bc8a92ef3bac4d40bcb455f033590363b4
Mohamed Sabour <mohamedsabour63@gmail.com>
2023-02-16 16:17:24 -0800

    start to coding with C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,7 @@
+#iclude<stdio.h>
+int main()
+{
+//string initialisationchar
+char mystr[] = "Programming is like building a multilingual puzzle;
+puts(mystr); //writing the string to stout
+return 0;
