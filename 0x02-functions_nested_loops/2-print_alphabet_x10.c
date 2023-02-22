b841bd71af3eb3550b07fe726ae63456a5371db8
Mohamed Sabour <mohamedsabour63@gmail.com>
2023-02-21 16:00:14 -0800

    ):

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,13 +7,13 @@ void  print_alphabet_x10(void)
 {
 	char b;
 	int a = 0;
-	while (a < 10)
-	{
-	a++;
+		while (a < 10)
+		{
+		a++;
 		for (b = 'a' ; b <= 'z' ; b++)
-	{
-		_putchar(b);
-	}
-		_putchar('\n');
-	}
+		{
+			_putchar(b);
+		}
+			_putchar('\n');
+		}
 }
